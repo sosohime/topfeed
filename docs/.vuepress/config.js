5396e8da3044ea1da8dfd469d5a884f9be2dcdfc
hardfist <1562502418@qq.com>
2018-08-21 18:29:32 +0800

    doc setup
    Change-Id: If21b25fd4b6d26d694e6294f3438886dc4bba0c8

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,33 @@
 module.exports = {
-	title: "Hello Topfeed",
-	description: "Just Playing around",
+	title: "Topfeed",
+	description: "SSR FrameWork",
+	base: "/",
+	dest: "./dist",
+
 	themeConfig: {
 		nav: [
-			{
-				text: "指南",
-				link: "/guide"
-			},
-			{
-				text: "API",
-				link: "/api"
-			},
-			{
-				text: "示例",
-				link: "/examples"
-			}
+			{ text: "指南", link: "/guide/" },
+			{ text: "API", link: "/api/" },
+			{ text: "教程", link: "/tutorials/" },
+			{ text: "GitHub", link: "https://github.com/TopFeed/topfeed" }
 		],
 		sidebar: {
 			"/guide/": [
 				{
 					title: "指南",
 					collapsable: false,
-					children: ["", "getting-started", "i18n"]
+					children: ["", "getting-started", "i18n", "advanced"]
 				}
 			]
+		},
+		lastUpdated: "Last Updated"
+	},
+
+	markdown: {
+		// options for markdown-it-anchor
+		anchor: { permalink: false },
+		config: md => {
+			md.use(require("markdown-it-katex"));
 		}
 	}
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,1 @@
-this is a doc
+# SSR FrameWork
